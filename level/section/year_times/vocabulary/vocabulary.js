2b9b08e74bf8bb02d8078dc5d841b35d95da6b31
argosdidit5963 <120153198+argosdidit@users.noreply.github.com>
2026-04-16 21:24:52 +0900

    Update vocabulary.js

--- level/section/year_times/vocabulary/vocabulary.js
+++ level/section/year_times/vocabulary/vocabulary.js
@@ -50,7 +50,7 @@
 
       const { label, color } = levelMap[levelParam] || levelMap["pre2"];
       header.style.backgroundColor = color;
-      header.querySelector("h1").textContent = `単語 ${label} ${year}年 ${times}回`;
+      header.querySelector("h1").textContent = `ワード ${label} ${year}年 ${times}回`;
 
       backLink.href = `../year_times.html?level=${levelParam}&field=${fieldParam}`;
 
